acef40d24422dcbad38ff2a949e1c16ec822c506
Olzhas Suleimen <amajisenpai@gmail.com>
2025-10-11 22:11:12 +0500

    Update example.

--- example/example.dart
+++ example/example.dart
@@ -4,8 +4,8 @@ import 'package:textwrap/textwrap.dart';
 
 void main() {
   var text = "Hello there, how are you this fine day?  I'm glad to hear it!";
-  // Hello there, how are you
-  // this fine day?
+  // Hello there, how are
+  // you this fine day?
   // I'm glad to hear it!
   print(wrap(text, width: 20).join('\n'));
 }
